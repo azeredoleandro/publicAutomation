5a4b97e3a9b45adb791e8d5380deba805865e601
Leandro Azeredo <51543866+azeredoleandro@users.noreply.github.com>
2024-02-21 20:58:38 +0000

    Code Review
    Reviewed the code and changed the way the service names are displayed.
    
    For example, instead of the generic "server" service, we know have LanManServer, the same name used since Windows NT.
    
    Ref: https://learn.microsoft.com/en-us/answers/questions/1271933/smb-on-windows-server-2019
    
    And for awareness, a Wikipedia result:
    
    https://en.wikipedia.org/wiki/Server_Message_Block#:~:text=On%20Microsoft%20Windows%2C%20the%20SMB,process%20communication%20(IPC)%20mechanism.

--- Windows/Get-Services.ps1
+++ Windows/Get-Services.ps1
@@ -1,30 +1,19 @@
 <#
-1. 
 This PowerShell script is designed to run on PowerShell version 3.0 and above.
 
 Este script do PowerShell foi projetado para ser executado na versão 3.0 ou superior do PowerShell.
-
-2. 
 It leverages WMI (Windows Management Instrumentation) to retrieve information about running services.
 
 Ele utiliza o WMI (Windows Management Instrumentation) para recuperar informações sobre serviços em execução.
-
-3. 
 This script should theoretically work on Windows Vista/Server 2008 and above where WMI is available.
 
 Este script teoricamente deve funcionar no Windows Vista/Server 2008 e superior, onde o WMI está disponível.
-
-4. 
 DISCLAIMER: This code is provided AS IS without any warranty or support.
 
 AVISOS LEGAIS: Este código é fornecido COMO ESTÁ, sem garantia ou suporte.
-
-5. 
-
 Have fun, I hope this code helps you and, may the force be with you!
 
 Divirta-se, espero que esse código lhe ajude e, que a força esteja com você!
-
 #>
 
 #Retrieve running services using WMI.
@@ -32,10 +21,10 @@ Divirta-se, espero que esse código lhe ajude e, que a força esteja com você!
 
 $services = Get-WmiObject Win32_Service | Where-Object { $_.State -eq 'Running' } | ForEach-Object {
     
-    #Retrieve service display name.
-    #Recupera o nome de exibição do serviço.
+    #Retrieve service name.
+    #Recupera o nome do serviço.
     
-    $serviceName = $_.DisplayName
+    $serviceName = $_.Name
     
     #Retrieve process ID associated with the service.
     #Recupera o ID do processo associado ao serviço.
@@ -47,17 +36,21 @@ $services = Get-WmiObject Win32_Service | Where-Object { $_.State -eq 'Running' 
     
     $process = Get-Process -Id $processId
 
-    #Create a custom object containing service name, start time, and process ID.
-    #Cria um objeto personalizado contendo nome do serviço, tempo de início e ID do processo.
+    #Create a custom object containing service name, start time, process ID, and process name.
+    #Cria um objeto personalizado contendo nome do serviço, tempo de início, ID do processo e nome do processo.
     
     [PSCustomObject]@{
         ServiceName = $serviceName
+        ProcessName = $process.ProcessName
         StartTime = $process.StartTime
         ProcessId = $processId
     }
 }
 
-# Output the list of services.
-# Exibe a lista de serviços.
+# Sort the list of services alphabetically by service name.
+# Classifica a lista de serviços em ordem alfabética pelo nome do serviço.
+$services = $services | Sort-Object ServiceName
 
+# Output the sorted list of services.
+# Exibe a lista classificada de serviços.
 $services
